4d02197724ad8658b17fc881a49c8bef68a7e5aa
daryoushjamshidi8080 <daryoushjamshidi8080@gmail.com>
2025-03-15 11:37:18 +0330

    comlate show features

--- website/resources/views/home/index.blade.php
+++ website/resources/views/home/index.blade.php
@@ -28,48 +28,9 @@
 
 @section('content')
 
-    <section class="card-area layout_padding">
-        <div class="container">
-            <div class="row gy-5">
-                <div class="col-md-4 col-sm-6 col-xs-6">
-                    <div class="card text-center">
-                        <div class="card-body">
-                            <div class="card-icon-wrapper">
-                                <i class="bi bi-telephone-fill fs-2 text-white card-icon"></i>
-                            </div>
-                            <p class="card-text fw-bold">لورم ایپسوم متن ساختگی</p>
-                            <p class="card-text">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده
-                                از طراحان گرافیک است. </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4 col-sm-6 col-xs-6">
-                    <div class="card text-center">
-                        <div class="card-body">
-                            <div class="card-icon-wrapper">
-                                <i class="bi bi-clock-fill fs-2 text-white card-icon"></i>
-                            </div>
-                            <p class="card-text fw-bold">لورم ایپسوم متن ساختگی</p>
-                            <p class="card-text">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده
-                                از طراحان گرافیک است. </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4 col-sm-6 col-xs-6">
-                    <div class="card text-center">
-                        <div class="card-body">
-                            <div class="card-icon-wrapper">
-                                <i class="bi bi-geo-alt-fill fs-2 text-white card-icon"></i>
-                            </div>
-                            <p class="card-text fw-bold">لورم ایپسوم متن ساختگی</p>
-                            <p class="card-text">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده
-                                از طراحان گرافیک است. </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+    <!-- features section -->
+    @include('home.features')
+    <!-- end features section -->
 
     <!-- food section -->
     <section class="food_section layout_padding-bottom">
